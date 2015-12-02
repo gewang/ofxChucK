6625b2faa8cc89ab875ab0939b438d116f2af17d
Ge Wang <ge@smule.com>
2015-12-01 20:55:33 -0800

    mvars added to VREntity

--- src/ofck.cpp
+++ src/ofck.cpp
@@ -17,24 +17,22 @@
 #include <stdio.h>
 #include <limits.h>
 
-// declaration of chugin constructor
-CK_DLL_CTOR(ofck_ctor);
-// declaration of chugin desctructor
-CK_DLL_DTOR(ofck_dtor);
 
 // VREntity
-CK_DLL_SFUN(vrentity_getLocation);
-CK_DLL_SFUN(vrentity_setLocation);
-CK_DLL_SFUN(vrentity_getRotation);
-CK_DLL_SFUN(vrentity_setRotation);
-CK_DLL_SFUN(vrentity_getScaling);
-CK_DLL_SFUN(vrentity_setScaling);
-CK_DLL_SFUN(vrentity_getRGB);
-CK_DLL_SFUN(vrentity_setRGB);
-CK_DLL_SFUN(vrentity_getRGBA);
-CK_DLL_SFUN(vrentity_setRGBA);
-CK_DLL_SFUN(vrentity_addEntity);
-CK_DLL_SFUN(vrentity_removeEntity);
+CK_DLL_CTOR(vrentity_ctor);
+CK_DLL_DTOR(vrentity_dtor);
+CK_DLL_MFUN(vrentity_getLocation);
+CK_DLL_MFUN(vrentity_setLocation);
+CK_DLL_MFUN(vrentity_getRotation);
+CK_DLL_MFUN(vrentity_setRotation);
+CK_DLL_MFUN(vrentity_getScaling);
+CK_DLL_MFUN(vrentity_setScaling);
+CK_DLL_MFUN(vrentity_getRGB);
+CK_DLL_MFUN(vrentity_setRGB);
+CK_DLL_MFUN(vrentity_getRGBA);
+CK_DLL_MFUN(vrentity_setRGBA);
+CK_DLL_MFUN(vrentity_addEntity);
+CK_DLL_MFUN(vrentity_removeEntity);
 
 // VR
 CK_DLL_SFUN(vr_object);
@@ -51,8 +49,11 @@ CK_DLL_SFUN(vr_getVec4);
 CK_DLL_SFUN(vr_displaySync);
 
 
-// this is a special offset reserved for Chugin internal data
-t_CKINT ofck_data_offset = 0;
+// internal data
+t_CKINT vrentity_offset_location = 0;
+t_CKINT vrentity_offset_rotation = 0;
+t_CKINT vrentity_offset_scaling = 0;
+t_CKINT vrentity_offset_rgba = 0;
 
 
 // defintion
@@ -92,126 +93,163 @@ DLL_QUERY ofck_query( Chuck_DL_Query * QUERY )
     // begin the class definition
     QUERY->begin_class(QUERY, "VREntity", "Object");
     {
-        // vec3 VREntity.location()
-        QUERY->add_sfun(QUERY, vrentity_getLocation, "vec3", "location");
-        QUERY->add_sfun(QUERY, vrentity_setLocation, "vec3", "location");
-        QUERY->add_arg(QUERY, "vec3", "loc");
+        // constructor
+        QUERY->add_ctor(QUERY, vrentity_ctor);
+        // destructor
+        QUERY->add_dtor(QUERY, vrentity_dtor);
+        
+        // add .location
+        vrentity_offset_location = QUERY->add_mvar( QUERY, "vec3", "location", FALSE );
+        if( vrentity_offset_location == CK_INVALID_OFFSET ) goto error;
 
-        // vec3 VREntity.rotation()
-        QUERY->add_sfun(QUERY, vrentity_getRotation, "vec3", "rotation");
-        QUERY->add_sfun(QUERY, vrentity_setRotation, "vec3", "rotation");
-        QUERY->add_arg(QUERY, "vec3", "rot");
-        
-        // vec3 VREntity.scaling()
-        QUERY->add_sfun(QUERY, vrentity_getScaling, "vec3", "scaling");
-        QUERY->add_sfun(QUERY, vrentity_setScaling, "vec3", "scaling");
-        QUERY->add_arg(QUERY, "vec3", "sca");
-        
-        // vec3 VREntity.rgb()
-        QUERY->add_sfun(QUERY, vrentity_getRGB, "vec3", "rgb");
-        QUERY->add_sfun(QUERY, vrentity_setRGB, "vec3", "rgb");
-        QUERY->add_arg(QUERY, "vec3", "col");
-        
-        // vec4 VREntity.rgb()
-        QUERY->add_sfun(QUERY, vrentity_getRGBA, "vec4", "rgba");
-        QUERY->add_sfun(QUERY, vrentity_setRGBA, "vec4", "rgba");
-        QUERY->add_arg(QUERY, "vec4", "rgba");
+        // add .rotation
+        vrentity_offset_rotation = QUERY->add_mvar( QUERY, "vec3", "rotation", FALSE );
+        if( vrentity_offset_location == CK_INVALID_OFFSET ) goto error;
+
+        // add .location
+        vrentity_offset_scaling = QUERY->add_mvar( QUERY, "vec3", "scaling", FALSE );
+        if( vrentity_offset_location == CK_INVALID_OFFSET ) goto error;
+
+        // add .location
+        vrentity_offset_rgba = QUERY->add_mvar( QUERY, "vec4", "rgba", FALSE );
+        if( vrentity_offset_location == CK_INVALID_OFFSET ) goto error;
+
+//        // vec3 VREntity.location()
+//        QUERY->add_mfun(QUERY, vrentity_getLocation, "vec3", "location");
+//        QUERY->add_mfun(QUERY, vrentity_setLocation, "vec3", "location");
+//        QUERY->add_arg(QUERY, "vec3", "loc");
+//
+//        // vec3 VREntity.rotation()
+//        QUERY->add_mfun(QUERY, vrentity_getRotation, "vec3", "rotation");
+//        QUERY->add_mfun(QUERY, vrentity_setRotation, "vec3", "rotation");
+//        QUERY->add_arg(QUERY, "vec3", "rot");
+//        
+//        // vec3 VREntity.scaling()
+//        QUERY->add_mfun(QUERY, vrentity_getScaling, "vec3", "scaling");
+//        QUERY->add_mfun(QUERY, vrentity_setScaling, "vec3", "scaling");
+//        QUERY->add_arg(QUERY, "vec3", "sca");
+//        
+//        // vec3 VREntity.rgb()
+//        QUERY->add_mfun(QUERY, vrentity_getRGB, "vec3", "rgb");
+//        QUERY->add_mfun(QUERY, vrentity_setRGB, "vec3", "rgb");
+//        QUERY->add_arg(QUERY, "vec3", "col");
+//        
+//        // vec4 VREntity.rgb()
+//        QUERY->add_mfun(QUERY, vrentity_getRGBA, "vec4", "rgba");
+//        QUERY->add_mfun(QUERY, vrentity_setRGBA, "vec4", "rgba");
+//        QUERY->add_arg(QUERY, "vec4", "rgba");
     }
     // end the class definition
     QUERY->end_class(QUERY);
 
-    // begin the class definition
-    QUERY->begin_class(QUERY, "VR", "Object");
-    {
-        // VRObject VR.object(name) // retrive objects
-        QUERY->add_sfun(QUERY, vr_object, "VRObject", "object");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "string", "name");
-
-        // int VR.setInt(key,value) // set
-        QUERY->add_sfun(QUERY, vr_setInt, "int", "setInt");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "string", "key");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "int", "value");
-
-        // float VR.setFloat(key,value) // set
-        QUERY->add_sfun(QUERY, vr_getInt, "int", "getInt");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "string", "key");
-
-        // float VR.setFloat(key,value) // set
-        QUERY->add_sfun(QUERY, vr_setFloat, "float", "setFloat");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "string", "key");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "int", "value");
-
-        // float VR.getFloat(key,value) // set
-        QUERY->add_sfun(QUERY, vr_getFloat, "float", "getFloat");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "string", "key");
-        
-        // float VR.setString(key,value) // set
-        QUERY->add_sfun(QUERY, vr_setFloat, "string", "setString");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "string", "key");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "string", "value");
-        
-        // string VR.getString(key,value) // set
-        QUERY->add_sfun(QUERY, vr_getString, "string", "getString");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "string", "key");
-
-        // vec3 VR.setVec3(key,value) // set
-        QUERY->add_sfun(QUERY, vr_setVec3, "vec3", "setVec3");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "string", "key");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "vec3", "value");
-        
-        // vec3 VR.getVec3(key,value) // set
-        QUERY->add_sfun(QUERY, vr_getString, "vec3", "getVec3");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "string", "key");
-
-        // vec4 VR.setVec4(key,value) // set
-        QUERY->add_sfun(QUERY, vr_setVec4, "vec4", "setVec4");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "string", "key");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "vec4", "value");
-        
-        // vec4 VR.getVec4(key,value) // set
-        QUERY->add_sfun(QUERY, vr_getString, "vec4", "getVec4");
-        // name of object to retrieve
-        QUERY->add_arg(QUERY, "string", "key");
-        
-        // Event VR.displaySync // event for display
-        QUERY->add_sfun(QUERY, vr_object, "Event", "displaySync");
-    }
-    // end the class definition
-    QUERY->end_class(QUERY);
+//    // begin the class definition
+//    QUERY->begin_class(QUERY, "VR", "Object");
+//    {
+//        // VRObject VR.object(name) // retrive objects
+//        QUERY->add_sfun(QUERY, vr_object, "VRObject", "object");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "string", "name");
+//
+//        // int VR.setInt(key,value) // set
+//        QUERY->add_sfun(QUERY, vr_setInt, "int", "setInt");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "string", "key");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "int", "value");
+//
+//        // float VR.setFloat(key,value) // set
+//        QUERY->add_sfun(QUERY, vr_getInt, "int", "getInt");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "string", "key");
+//
+//        // float VR.setFloat(key,value) // set
+//        QUERY->add_sfun(QUERY, vr_setFloat, "float", "setFloat");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "string", "key");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "int", "value");
+//
+//        // float VR.getFloat(key,value) // set
+//        QUERY->add_sfun(QUERY, vr_getFloat, "float", "getFloat");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "string", "key");
+//        
+//        // float VR.setString(key,value) // set
+//        QUERY->add_sfun(QUERY, vr_setFloat, "string", "setString");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "string", "key");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "string", "value");
+//        
+//        // string VR.getString(key,value) // set
+//        QUERY->add_sfun(QUERY, vr_getString, "string", "getString");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "string", "key");
+//
+//        // vec3 VR.setVec3(key,value) // set
+//        QUERY->add_sfun(QUERY, vr_setVec3, "vec3", "setVec3");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "string", "key");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "vec3", "value");
+//        
+//        // vec3 VR.getVec3(key,value) // set
+//        QUERY->add_sfun(QUERY, vr_getString, "vec3", "getVec3");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "string", "key");
+//
+//        // vec4 VR.setVec4(key,value) // set
+//        QUERY->add_sfun(QUERY, vr_setVec4, "vec4", "setVec4");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "string", "key");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "vec4", "value");
+//        
+//        // vec4 VR.getVec4(key,value) // set
+//        QUERY->add_sfun(QUERY, vr_getString, "vec4", "getVec4");
+//        // name of object to retrieve
+//        QUERY->add_arg(QUERY, "string", "key");
+//        
+//        // Event VR.displaySync // event for display
+//        QUERY->add_sfun(QUERY, vr_object, "Event", "displaySync");
+//    }
+//    // end the class definition
+//    QUERY->end_class(QUERY);
     
     // wasn't that a breeze?
     return TRUE;
+    
+error:
+    QUERY->end_class(QUERY);
+    
+    // done
+    return FALSE;
 }
 
 
-CK_DLL_SFUN(vrentity_getLocation)
+CK_DLL_CTOR(vrentity_ctor)
 {
+    // nothing for now
 }
 
-CK_DLL_SFUN(vrentity_setLocation);
-CK_DLL_SFUN(vrentity_getRotation);
-CK_DLL_SFUN(vrentity_setRotation);
-CK_DLL_SFUN(vrentity_getScaling);
-CK_DLL_SFUN(vrentity_setScaling);
-CK_DLL_SFUN(vrentity_getRGB);
-CK_DLL_SFUN(vrentity_setRGB);
-CK_DLL_SFUN(vrentity_getRGBA);
-CK_DLL_SFUN(vrentity_setRGBA);
-CK_DLL_SFUN(vrentity_addEntity);
-CK_DLL_SFUN(vrentity_removeEntity);
+CK_DLL_DTOR(vrentity_dtor)
+{
+    // nothing for now
+}
+
+//CK_DLL_SFUN(vrentity_getLocation)
+//{
+//}
+//
+//CK_DLL_SFUN(vrentity_setLocation);
+//CK_DLL_SFUN(vrentity_getRotation);
+//CK_DLL_SFUN(vrentity_setRotation);
+//CK_DLL_SFUN(vrentity_getScaling);
+//CK_DLL_SFUN(vrentity_setScaling);
+//CK_DLL_SFUN(vrentity_getRGB);
+//CK_DLL_SFUN(vrentity_setRGB);
+//CK_DLL_SFUN(vrentity_getRGBA);
+//CK_DLL_SFUN(vrentity_setRGBA);
+//CK_DLL_SFUN(vrentity_addEntity);
+//CK_DLL_SFUN(vrentity_removeEntity);
 
